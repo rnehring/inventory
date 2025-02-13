91b8698ee2487a3f7d0e3f8ded1589c62baf48b3
Ryan Nehring <nehring.ryan@gmail.com>
2025-02-12 23:45:14 -0500

    Updating count controller query

--- app/Http/Controllers/CountController.php
+++ app/Http/Controllers/CountController.php
@@ -16,10 +16,51 @@ class CountController extends Controller
     public function getPart(Request $request){
 
         $partData = DB::select('
-            SELECT *
-            FROM inventory
+            SELECT
+                id,
+                tag,
+                part,
+                part_description,
+                bin,
+                description,
+                company,
+                lot_number,
+                serial_number,
+                count,
+                user,
+                uom,
+                by_weight,
+                expected_qty,
+                standard_cost,
+                date_counted,
+                time_counted,
+                cost_expected,
+                cost_counted,
+                ROUND(cost_counted - cost_expected, 2) AS plus_minus
+            FROM(
+                SELECT
+                    id,
+                    tag,
+                    part,
+                    part_description,
+                    bin,
+                    description,
+                    company,
+                    lot_number,
+                    serial_number,
+                    count,
+                    user,
+                    uom,
+                    by_weight,
+                    expected_qty,
+                    standard_cost,
+                    date_counted,
+                    time_counted,
+                    ROUND(standard_cost * expected_qty, 2) AS cost_expected,
+                    ROUND(standard_cost * count, 2) AS cost_counted
+                FROM inventory) AS INV
             WHERE part = ? AND bin = ?',
-            [$request->part, $request->bin]);
+            [$request->data['part'], $request->data['bin']]);
 
         return json_encode($partData);
     }
