92f48dab7f11ecea85df8b0daec7526c09606dd8
rnehring <nehring.ryan@gmail.com>
2025-10-23 16:17:40 -0400

    Endurance fix

--- resources/views/data/index.blade.php
+++ resources/views/data/index.blade.php
@@ -21,7 +21,7 @@
                 <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white mt-8">
 
                     @php
-                    $companies = ['10', '20', '30', '40', '50', 'CC0', 'G50', 'PV0'];
+                    $companies = ['10', '20', '30', '40', '50', 'CC0', 'GS0', 'PV0'];
                     foreach ($companies as $company) {
                         echo '<li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">';
                         echo '<div class="flex items-center ps-3">';
