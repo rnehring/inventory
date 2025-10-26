e107fb5a31814f7a8ac13cf3dbb9838834ccf95b
rnehring <nehring.ryan@gmail.com>
2025-10-26 00:44:59 -0400

    No Tag Data Calculations Added

--- resources/views/data/company-data.blade.php
+++ resources/views/data/company-data.blade.php
@@ -101,8 +101,12 @@
                     No Tag Total : {{ FunctionController::formatCurrency($noTagTotal) }}
                 </div>
                 <div
+                    class="inline-block focus:outline-none text-white bg-violet-700 hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-violet-600 dark:hover:bg-violet-700 dark:focus:ring-violet-900">
+                    Total Plus/Minus (Taggged Inventory) : {{ FunctionController::formatCurrency($totalPlusMinus) }}
+                </div>
+                <div
                     class="inline-block focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                    Total Plus/Minus : {{ FunctionController::formatCurrency($totalPlusMinus) }}
+                    Total Plus/Minus (With No Tag): {{ FunctionController::formatCurrency($totalPlusMinus + $noTagTotal) }}
                 </div>
             </div>
         </div>
