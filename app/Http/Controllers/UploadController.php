435fff56dde7334536bd0532be9fd3c558b91087
rnehring <nehring.ryan@gmail.com>
2025-12-23 04:55:46 -0500

    Upload Factor Fixes

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -264,15 +264,17 @@ class UploadController extends FunctionController
         DB::statement($setTopEighty);
 
         DB::unprepared("
-            DROP TRIGGER IF EXISTS calculate_precount_inventory_costs_before_update
+            DROP TRIGGER IF EXISTS calculate_precount_inventory_costs_before_update;
 
-            CREATE DEFINER=`rnehring`@`%` TRIGGER `calculate_inventory_costs_before_update` BEFORE UPDATE ON `inventory_precount` FOR EACH ROW
+            CREATE DEFINER=`rnehring`@`%` TRIGGER `calculate_inventory_costs_before_update`
+            BEFORE UPDATE ON `inventory_precount`
+            FOR EACH ROW
             BEGIN
                 SET NEW.counted = 1;
                 SET NEW.cost_expected = NEW.expected_qty * NEW.standard_cost;
                 SET NEW.cost_counted = NEW.count * NEW.standard_cost;
                 SET NEW.plus_minus = NEW.cost_counted - NEW.cost_expected;
-            END
+            END;
         ");
         return view('upload.savedpre');
     }
