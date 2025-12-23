09c6aac2f652a6e12ee30f66df51203e81176be6
rnehring <nehring.ryan@gmail.com>
2025-12-23 05:48:29 -0500

    Upload Factor Fixes

--- resources/js/inventory-precount.js
+++ resources/js/inventory-precount.js
@@ -41,7 +41,7 @@ function getPart(event) {
                 let bin = makeCell(`${part['bin']}`, 'text-center');
                 let uom = makeCell(`${part['uom']}`, 'text-center');
                 let count = makeCell(`<input type='text' name='count' id='count${part['id']}' class='text-right px-2 py-0 mx-auto block rounded-sm border-gray-600' value='${part['count']}' />`);
-                let bin_verified = makeCell(`<input type='checkbox' class='mx-auto block px-2' id='bin_verified' ${part['bin_verified'] === 1 ? 'checked />' : '/>'}`);
+                let bin_verified = makeCell(`<input type='checkbox' class='mx-auto block px-2' id='bin_verified${part['id']}' ${part['bin_verified'] === 1 ? 'checked />' : '/>'}`);
                 let by_weight = makeCell(`<input type='checkbox' class='mx-auto block' id='by_weight' ${part['by_weight'] === 1 ? 'checked />' : '/>'}`);
                 let lot_number = makeCell(`${part['lot_number']}`);
                 let serial_number = makeCell(`${part['serial_number']}`);
@@ -55,9 +55,16 @@ function getPart(event) {
 
                 let save_button = makeCell(`<button type="button" data-part_id = '${part['id']}' name='saveCount' class="justify-center px-3 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><svg id='Layer_1' class='w-3 h-3 text-white me-2' fill='currentColor' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'><path d='M22,4h-2v6c0,0.552-0.448,1-1,1h-9c-0.552,0-1-0.448-1-1V4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18  c1.105,0,2-0.895,2-2V8L22,4z M22,24H8v-6c0-1.105,0.895-2,2-2h10c1.105,0,2,0.895,2,2V24z'/><rect height="5" width="2" x="16" y="4"/></svg>Save</button>`, 'text-center');
                 save_button.addEventListener('click', function(event){
+                    let bin_verified = document.getElementById('bin_verified' + part['id']);
+                    if (bin_verified.checked) {
+                        let verified = 1;
+                    } else {
+                        let verified = 0;
+                    }
                     axios.post('/update-precount', {
                         count: document.getElementById('count'+part['id']).value,
-                        part: part['id']
+                        part: part['id'],
+                        bin_verified: verified
                     })
                         .then(function (response) {
                             let row = document.getElementById('row'+part['id']);
