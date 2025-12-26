203aaffe9f0b7605a7cd0f9e53a6604b30d3ac6e
rnehring <nehring.ryan@gmail.com>
2025-12-26 04:27:27 -0500

    Update VITE

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/dashboard.js','resources/js/inventory-count.js', 'resources/js/location-count.js', 'resources/js/no-tag.js', 'resources/js/admin-data.js', 'resources/js/manager-login.js', 'resources/js/employee-login.js', 'resources/js/users.js','resources/js/inventory-precount.js', 'resources/js/location-precount.js'], 'resources/js/precount-upload.js'], 'resources/js/inventory-upload.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/dashboard.js','resources/js/inventory-count.js', 'resources/js/location-count.js', 'resources/js/no-tag.js', 'resources/js/admin-data.js', 'resources/js/manager-login.js', 'resources/js/employee-login.js', 'resources/js/users.js','resources/js/inventory-precount.js', 'resources/js/location-precount.js', 'resources/js/precount-upload.js', 'resources/js/inventory-upload.js'],
             refresh: true,
         }),
     ],
