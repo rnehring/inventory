3f4b3ce1e7cf24e920e4a7a236ed07fdb3bda0c0
rnehring <nehring.ryan@gmail.com>
2025-12-30 11:24:42 -0500

    No Tag Update Fix

--- resources/js/no-tag.js
+++ resources/js/no-tag.js
@@ -21,6 +21,19 @@ const partFieldDiv = partField.parentElement;
 const binField = document.getElementById("bins");
 const binFieldDiv = binField.parentElement;
 
+const loadingButton = `<button disabled type="button" class="text-white bg-brand inline-flex items-center box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+<svg aria-hidden="true" role="status" class="w-4 h-4 me-2 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
+<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
+</svg>
+Loading...
+</button>`;
+
+const disabledClasses = "dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-700 box-border border dark:border-gray-900 shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none";
+
+const enabledClasses = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-4";
+
+
 function buildError(field, message){
     let fieldParent = field.parentElement;
     let error = document.createElement("p");
@@ -30,6 +43,18 @@ function buildError(field, message){
     error.style.fontWeight = "bold";
     error.textContent = message;
     fieldParent.appendChild(error);
+}
+
+function enableSubmit(){
+    submitButton.disabled = false;
+    submitButton.innerHTML = "Add Part";
+    submitButton.classList = enabledClasses;
+}
+
+function disableSubmit(){
+    submitButton.disabled = true;
+    submitButton.innerHTML = "Add Part";
+    submitButton.classList += " " + disabledClasses;
 }
 
 function removeErrors(){
@@ -65,23 +90,24 @@ function canSubmit() {
             let canSubmit = false;
 
             if( tracking.data[0].track_lot == 1 && lot.length > 0 ) {
+                enableSubmit()
                 canSubmit = true;
             } else if( tracking.data[0].track_lot == 0) {
+                enableSubmit()
                 canSubmit = true;
             } else {
                 buildError(lotField, "This Part Requires a Lot Number");
-                submitButton.disabled = false;
-                submitButton.innerHTML = "Add Part";
+                disableSubmit();
                 return false;
             }
 
-            console.log(serial)
             if ( serial.data == "false" ) {
+                enableSubmit();
                 canSubmit = true;
             } else {
                 buildError(serialField, "Serial Number must be unique and is already in use.");
-                submitButton.disabled = false;
-                submitButton.innerHTML = "Add Part";
+                disableSubmit();
+                serialField.addEventListener("change", enableSubmit);
                 return false;
             }
 
@@ -92,8 +118,6 @@ function canSubmit() {
 }
 
 const performSearch = (list, query) => {
-    console.log(query);
-    console.log(list);
     if ( list.includes(query) ) {
         return true;
     }
@@ -102,8 +126,6 @@ const performSearch = (list, query) => {
 
 function checkIsValid(field, list){
     removeErrors();
-    console.log(list);
-    console.log(field);
     let errId = field.name + "Error";
     let fieldError = document.getElementById(errId);
     let fieldParent = field.parentElement;
@@ -116,7 +138,7 @@ function checkIsValid(field, list){
         if( fieldError ){
             fieldError.remove();
         }
-        submitButton.disabled = false;
+        enableSubmit();
         return true;
     } else {
         field.style.backgroundColor = "#c40000";
@@ -125,7 +147,7 @@ function checkIsValid(field, list){
         }
         let errMsg = errorMsgStart + "Not Found.";
         buildError(field, errMsg);
-        submitButton.disabled = true;
+        disableSubmit();
         return false;
     }
 }
@@ -150,6 +172,7 @@ lotField.onchange = event => {
         let lotFieldError = document.getElementById('lot_numberError');
         if( lotFieldError ){
             lotFieldDiv.removeChild(lotFieldError);
+            enableSubmit();
         }
     }
 }
@@ -158,6 +181,9 @@ lotField.onchange = event => {
 
 submitButton.addEventListener("click", function(event) {
     event.preventDefault();
+    if(submitButton.disabled){
+        return false;
+    }
     submitButton.textContent = 'Processing...';
     submitButton.disabled = true;
     removeErrors();
